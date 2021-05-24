4b62ee4387e89c3f427ec52eb0f4e76567cdd6c3
Luis Alfredo Elias Humar <luiseliashumar@gmail.com>
2021-05-24 11:20:40 -0700

    Final details

--- plagiarism_detector/task_group/views.py
+++ plagiarism_detector/task_group/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 import datetime
 from .models import TaskGroup
-from essays.models import EssayPlagiarism,Essay
+from essays.models import EssayPlagiarism, Essay
 
 
 # Create your views here.
 
 def index(request):
     if request.user.is_authenticated:
-       
+
         rows = []
         groups = TaskGroup.objects.filter(user=request.user.id)
         for group in groups:
             essays = Essay.objects.filter(task_group=group.id)
             rows.append({
-                'id':group.id,
+                'id': group.id,
                 'name': group.name,
                 'essays_size': len(essays),
                 'created_at': group.created_at
@@ -28,42 +28,40 @@ def index(request):
 
 
 def create(request):
-    if request.user.is_authenticated :
+    if request.user.is_authenticated:
         return render(request, 'task_group_form.html', {
             'title': 'Crear Grupo de Tarea',
             'index': -1
         })
-    else :
+    else:
         return redirect('login')
 
 
 def edit(request, group_index: int):
-    if request.user.is_authenticated :
+    if request.user.is_authenticated:
         group = TaskGroup.objects.get(id=group_index)
         return render(request, 'task_group_form.html', {
             'title': 'Editar Grupo de Tareas',
             'group': group,
             'index': group_index
         })
-    else :
+    else:
         return redirect('login')
 
 
 def delete(request, group_index: int):
-    if request.user.is_authenticated :
+    if request.user.is_authenticated:
         instance = TaskGroup.objects.get(id=group_index)
         EssayPlagiarism.objects.filter(task_group=group_index).delete()
         instance.delete()
         messages.success(request, 'Grupo de Tarea Eliminado Correctamente')
         return redirect('task_group.index')
-    else :
+    else:
         return redirect('login')
 
 
-
-
 def save(request):
-    if request.user.is_authenticated :
+    if request.user.is_authenticated:
         id = int(request.POST['group_id'])
         now = datetime.date.today()
         if id == -1:
@@ -77,15 +75,16 @@ def save(request):
 
         else:
             task_group = TaskGroup.objects.filter(id=id).first()
-            task_group.name=request.POST['name']
-            task_group.created_at=now
+            task_group.name = request.POST['name']
+            task_group.created_at = now
             task_group.save()
             messages.success(request, 'Grupo de Tarea Editado Correctamente')
         return redirect('task_group.index')
-    else :
+    else:
         return redirect('login')
 
-def verify(request,group_index: int):
+
+def verify(request, group_index: int):
     EssayPlagiarism.objects.all().delete()
     task_group = TaskGroup.objects.filter(id=group_index).first()
     essays = Essay.objects.filter(task_group=group_index)
@@ -95,7 +94,7 @@ def verify(request,group_index: int):
     for essay in essays:
         for essay2 in essays:
             if essay.id != essay2.id:
-                combination = [essay.id,essay2.id]
+                combination = [essay.id, essay2.id]
                 if not combination in combinations:
                     plagiarism = EssayPlagiarism(
                         essay1=essay.id,
@@ -104,12 +103,10 @@ def verify(request,group_index: int):
                     )
                     plagiarism.compute_jaccard()
                     plagiarism.save()
-                    
+
                     combination = combination[::-1]
-                    
                     combinations.append(combination)
-    
-   
+
     results = []
     essay_plagiarisms = EssayPlagiarism.objects.filter(task_group=group_index)
     for essay_plagiarism in essay_plagiarisms:
@@ -117,14 +114,14 @@ def verify(request,group_index: int):
         essay2 = Essay.objects.get(id=essay_plagiarism.essay2)
         results.append({
             'id': essay_plagiarism.id,
-            'essay1_id' :essay1.id,
+            'essay1_id': essay1.id,
             'essay1_name': essay1.title,
-            'essay1_author':essay1.author.name,
-            'essay2_id' :essay2.id,
+            'essay1_author': essay1.author.name,
+            'essay2_id': essay2.id,
             'essay2_name': essay2.title,
-            'essay2_author':essay2.author.name,
+            'essay2_author': essay2.author.name,
             'avg': essay_plagiarism.get_plagiarism,
-            'verified_on' : essay_plagiarism.date
+            'verified_on': essay_plagiarism.date
         })
 
     return render(request, 'palgiarism.html', {
